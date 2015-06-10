af89a57f905c6f55be1a9f0f576ff5d5a255156e
SpiritWolf2015 <522414019@qq.com>
2015-06-10 17:46:31 +0800

    1.1
    根据快速入门做的，简单的对话流程。触发代码改LUA变量，完成任务。

---
+++ Assets/_GZC/GzcQuickStart/GzcQuickStart/ConditionObserverSetVariable_BehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using PixelCrushers.DialogueSystem;
+
+public class ConditionObserverSetVariable_BehaviourScript : MonoBehaviour {
+
+	
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+     
+	}
+
+    void OnTriggerEnter (Collider other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("SetVariable->PlayerIn为真");
+            // 设置LUA变量
+            DialogueLua.SetVariable("player_in", true);
+        }
+    }
+
+
+}
---
+++ Assets/_GZC/GzcQuickStart/GzcQuickStart/ShowAlertBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using PixelCrushers.DialogueSystem;
+
+public class ShowAlertBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("脚本里写的提示消息");
+            DialogueManager.ShowAlert("脚本里写的提示消息");
+        }
+	}
+}
