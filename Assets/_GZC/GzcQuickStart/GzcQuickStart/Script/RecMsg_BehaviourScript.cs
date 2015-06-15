970fecce337a0975989dc9bf4d9a2c544f898c13
SpiritWolf2015 <522414019@qq.com>
2015-06-15 17:26:50 +0800

    1.3
    触发器触发与NPC对话。

---
+++ Assets/_GZC/GzcQuickStart/GzcQuickStart/Script/RecMsg_BehaviourScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecMsg_BehaviourScript : MonoBehaviour {
+
+    void RecMsg1 (string str) {
+        Debug.Log(this + ", 参数str = " + str);
+    }
+
+    void RecMsg2 ( ) {
+        Debug.Log(this);
+    }
+
+}
---
+++ Assets/_GZC/GzcQuickStart/GzcQuickStart/Script/ShowAlertBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using PixelCrushers.DialogueSystem;
+
+public class ShowAlertBehaviourScript : MonoBehaviour {
+
+    [Range(0.1F, 5F)]
+    public float m_tipSecond = 3F;
+
+	// Use this for initialization
+	void Start () {
+        tip( );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.G)) {
+            tip( );
+        }
+	}
+
+    void tip ( ) {
+        const string s = "C#脚本里写的提示消息，还可以LUA写提示消息";
+        Debug.Log(s);
+        // 第2个参数是显示秒数
+        DialogueManager.ShowAlert(s, m_tipSecond);
+    }
+
+}
